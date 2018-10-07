f8d10a303176ceb977d3d308e6e0d1d242538d55
shaun <shaun@cipher>
2018-10-07 04:39:35 +0200

    Initial commit

---
+++ src/MovieRental/Database.java
@@ -0,0 +1,244 @@
+package MovieRental;
+
+/**
+ * @author Tshepo Sepadile
+ * @author Banele Mlamleli
+ */
+import static MovieRental.ReadFile.*;
+import java.awt.HeadlessException;
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Database {
+
+    public static Connection connect;
+    public static Statement stmnt;
+    public static PreparedStatement prepStmnt;
+    public static ResultSet resultSet;
+    
+    //The arraylist below will store all the data read from the database from each table created
+    public static ArrayList<Customer> arrayListSelectAllCustomers = new ArrayList<>();
+    public static ArrayList<DVD> arrayListSelectAllMovies = new ArrayList<>();
+    public static ArrayList<Rental> arrayListSelectAllRentals = new ArrayList<>();
+    
+
+    //Establish connection to the databse using UCAnAccess
+    public static void Connect() {
+        try {
+            String dbURL = "jdbc:ucanaccess://F:\\MovieRental\\src\\Assets\\MovieDatabase.accdb";
+//            String username = "";
+//            String password = "";
+            String driverName = "net.ucanaccess.jdbc.UcanaccessDriver";
+            System.out.println("About to Load the JDBC Driver....");
+            Class.forName(driverName);
+            connect = DriverManager.getConnection(dbURL);
+            System.out.println("Successfully connected to database.");            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage()+"\nUnable to connect to database.", "Error", JOptionPane.ERROR_MESSAGE);
+            System.out.println(e.getMessage()+"\nUnable to connect to database.");
+        } catch(ClassNotFoundException cnfe){
+            JOptionPane.showMessageDialog(null, cnfe.getMessage()+"\nUnable to connect to database.", "Error", JOptionPane.ERROR_MESSAGE);
+            System.out.println(cnfe.getMessage()+"\nUnable to connect to database.");
+        }
+    }
+    
+    //Dropping the table it the are created already
+    public static void dropTables() {
+        try {
+            String dropCustomerTable = "DROP TABLE CUSTOMER";
+            String dropDvdTable = "DROP TABLE DVD";
+            String dropRentalTable = "DROP TABLE RENTAL";
+            stmnt = connect.createStatement();
+            stmnt.execute(dropCustomerTable);
+            stmnt.execute(dropDvdTable);
+            stmnt.execute(dropRentalTable);
+
+            System.out.println("Tables dropped");
+        } catch (SQLException sqle) {
+            System.out.println(sqle.fillInStackTrace());
+        } catch (NullPointerException npe) {
+            System.out.println(npe.fillInStackTrace());
+        }
+    }
+        
+    public static void createTables(Connection conn) {
+        try {
+            //Creating tables
+            System.out.println("Creating CUSTOMER table.");
+            String create_table_cust = "create table CUSTOMER (custNumber INTEGER CONSTRAINT customer_custnum_pk PRIMARY KEY, "
+                    + "firstName VARCHAR(50) CONSTRAINT customer_fName_nn NOT NULL, "
+                    + "surname VARCHAR(50) CONSTRAINT customer_surname_nn NOT NULL, "
+                    + "phoneNum VARCHAR(20) CONSTRAINT customer_phoneNum_uk UNIQUE, " /*UNIQUE*/
+                    + "credit DOUBLE, "
+                    + "Can_Rent VARCHAR(5) CONSTRAINT customer_can_rent_nn NOT NULL)";
+            
+            System.out.println("Creating DVD table.");
+            String create_table_dvd = "create table DVD(dvdNumber INTEGER CONSTRAINT dvd_dvdNum_pk PRIMARY KEY, "
+                    + "title VARCHAR(50) CONSTRAINT dvd_title_uk UNIQUE, " /*UNIQUE*/
+                    + "category VARCHAR(50) CONSTRAINT dvd_category_nn NOT NULL, "
+                    + "price DOUBLE, "
+                    + "New_Release VARCHAR(5) CONSTRAINT dvd_new_release_nn NOT NULL, "
+                    + "Available_For_Rental VARCHAR(5) CONSTRAINT dvd_available_for_rental_nn NOT NULL)";
+            
+            System.out.println("Creating RENTAL table.");
+            String create_table_rental = "create table RENTAL(rentalNumber INTEGER CONSTRAINT rental_rentalNum_pk PRIMARY KEY, "
+                    + "dateRented VARCHAR(12) CONSTRAINT rental_drented_nn NOT NULL, "
+                    + "dateReturned VARCHAR(12) CONSTRAINT rental_dreturned_nn NOT NULL, "
+                    + "custNumber INTEGER CONSTRAINT rental_custNum_fk REFERENCES CUSTOMER(custNumber), "
+                    + "dvdNumber INTEGER CONSTRAINT rental_dvdNum_fk REFERENCES DVD(dvdNumber), "
+                    + "totalPenaltyCost DOUBLE)";
+            stmnt = connect.createStatement();
+//            stmnt.executeUpdate("DROP TABLE RENTAL"); //deleting rental table if it exist already
+//            stmnt.executeUpdate("DROP TABLE CUSTOMER"); //deleting customer table if it exist already
+//            stmnt.executeUpdate("DROP TABLE DVD"); //deleting dvd table if it exist already            
+            stmnt.executeUpdate(create_table_cust); //creating customer table
+            stmnt.executeUpdate(create_table_dvd); //creating dvd table
+            stmnt.executeUpdate(create_table_rental); //creating rental table
+            JOptionPane.showMessageDialog(null,"Tables were created successfully.","Success",JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("Tables were created successfully.");
+
+            //stmnt.close();
+            //connect.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,e.getMessage()+"\nError!! Database was not created","Error",JOptionPane.ERROR_MESSAGE);
+            System.out.println(e.getMessage()+"\nError!! Database was not created");
+        }
+    }
+        
+    //populate CUSTOMER table with data from serialized file
+    public static void insertCustomerData() {
+        try {
+            String insertDetails = "INSERT INTO CUSTOMER(custNumber, firstName, surname, phoneNum, credit, Can_Rent) VALUES(?, ?, ?, ?, ?, ?)";
+            for(int a = 0; a < customerArraylist.size(); a++){
+                prepStmnt = connect.prepareStatement(insertDetails);
+                prepStmnt.setInt(1, customerArraylist.get(a).getCustNumber());
+                prepStmnt.setString(2, customerArraylist.get(a).getName());
+                prepStmnt.setString(3, customerArraylist.get(a).getSurname());
+                prepStmnt.setString(4, customerArraylist.get(a).getPhoneNum());
+                prepStmnt.setDouble(5, customerArraylist.get(a).getCredit());
+                prepStmnt.setString(6, Boolean.toString(customerArraylist.get(a).canRent()));
+                prepStmnt.executeUpdate();
+            }
+            System.out.println("Customer data has been recorded successfully");
+        } catch (HeadlessException | SQLException error) {
+            JOptionPane.showMessageDialog(null, "Unable to insert Customer data from serialized file\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    //populate DVD table with data from serialized file
+    public static void insertDvdData() {
+        try {
+            String insertDetails = "INSERT INTO DVD(dvdNumber, title, category, price, New_Release, Available_For_Rental) VALUES(?, ?, ?, ?, ?, ?)";
+            for(int a = 0; a < dvdArraylist.size(); a++){
+                prepStmnt = connect.prepareStatement(insertDetails);
+                prepStmnt.setInt(1, dvdArraylist.get(a).getDvdNumber());
+                prepStmnt.setString(2, dvdArraylist.get(a).getTitle());
+                prepStmnt.setString(3, dvdArraylist.get(a).getCategory());
+                prepStmnt.setDouble(4, dvdArraylist.get(a).getPrice());
+                prepStmnt.setString(5, Boolean.toString(dvdArraylist.get(a).isNewRelease()));
+                prepStmnt.setString(6, Boolean.toString(dvdArraylist.get(a).isAvailable()));
+                prepStmnt.executeUpdate();
+            }
+            System.out.println("Dvd data has been recorded successfully");
+        } catch (HeadlessException | SQLException error) {
+            JOptionPane.showMessageDialog(null, "Unable to insert Movie data from serialized file\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    //populate RENTAL table with data from serialized file
+    public static void insertRentalData() {
+        try {
+            String insertDetails = "INSERT INTO RENTAL(rentalNumber, dateRented, dateReturned, custNumber, dvdNumber, totalPenaltyCost) VALUES(?, ?, ?, ?, ?, ?)";
+            for(int a = 0; a < rentalArraylist.size(); a++){
+                prepStmnt = connect.prepareStatement(insertDetails);
+                prepStmnt.setInt(1, rentalArraylist.get(a).getRentalNumber());
+                prepStmnt.setString(2, rentalArraylist.get(a).getDateRented());
+                prepStmnt.setString(3, rentalArraylist.get(a).getDateReturned());
+                prepStmnt.setInt(4, rentalArraylist.get(a).getCustNumber());
+                prepStmnt.setInt(5, rentalArraylist.get(a).getDvdNumber());
+                prepStmnt.setDouble(6, rentalArraylist.get(a).getTotalPenaltyCost());
+                prepStmnt.executeUpdate();
+            }
+            System.out.println("Rental data has been recorded successfully");
+        } catch (HeadlessException | SQLException error) {
+            JOptionPane.showMessageDialog(null, "Unable to insert Rental data from serialized file\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    //Selecting all the data about customer from the CUSTOMER table
+    public static void selectAllCustomers(){
+        try {
+            stmnt = connect.createStatement();
+            ResultSet allCustomers = stmnt.executeQuery("SELECT * FROM CUSTOMER");
+            if (allCustomers != null) {
+                while (allCustomers.next()){
+                    arrayListSelectAllCustomers.add(new Customer(allCustomers.getInt(1), allCustomers.getString(2), allCustomers.getString(3), allCustomers.getString(4), allCustomers.getDouble(5), Boolean.parseBoolean(allCustomers.getString(6))));
+                }
+            }
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, "Unable to read all registered customer users\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    //Selecting all the data about the movies from the DVD table
+    public static void selectAllMovies() {
+        try {
+            stmnt = connect.createStatement();
+            ResultSet allMovies = stmnt.executeQuery("SELECT * FROM DVD");
+            int categoryValue = 0;
+            if (allMovies != null) {
+                while (allMovies.next()) {
+                    switch(allMovies.getString(3)) {
+                        case "horror":categoryValue = 1;break;
+                        case "Sci-fi":categoryValue = 2;break;
+                        case "Drama":categoryValue = 3;break;
+                        case "Romance":categoryValue = 4;break;
+                        case "Comedy":categoryValue = 5;break;
+                        case "Action":categoryValue = 6;break;
+                        case "Cartoon":categoryValue = 7;break;
+                    }
+                    arrayListSelectAllMovies.add(new DVD(allMovies.getInt(1), allMovies.getString(2), categoryValue, Boolean.parseBoolean(allMovies.getString(4)), Boolean.parseBoolean(allMovies.getString(5))));
+                }
+            }
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, "Unable to read all registered movies\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    //Selecting all the data about rentals from the RENTAL table
+    public static void selectAllRental() {
+        try {
+            stmnt = connect.createStatement();
+            ResultSet allRentals = stmnt.executeQuery("SELECT * FROM RENTAL");
+            if (allRentals != null) {
+                while (allRentals.next()){
+                    arrayListSelectAllRentals.add(new Rental(allRentals.getInt(1), allRentals.getString(2), allRentals.getString(3), allRentals.getInt(4), allRentals.getInt(5)));
+                }
+            }
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, "Unable to read all registered rentals\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    //Adding new customer into the CUSTOMER table
+    public void addCustomer(int custNumber, String firstName, String surname, String phoneNum, double credit, String Can_Rent) {
+        try {
+            String addCustomerDetails = "INSERT INTO CUSTOMER(custNumber, firstName, surname, phoneNum, credit, Can_Rent) VALUES(?, ?, ?, ?, ?, ?)";
+            for (int a = 0; a < customerArraylist.size(); a++) {
+                prepStmnt = connect.prepareStatement(addCustomerDetails);
+                prepStmnt.setInt(1, custNumber);
+                prepStmnt.setString(2, firstName);
+                prepStmnt.setString(3, surname);
+                prepStmnt.setString(4, phoneNum);
+                prepStmnt.setDouble(5, credit);
+                prepStmnt.setString(6, Can_Rent);
+                prepStmnt.executeUpdate();
+            }
+            JOptionPane.showMessageDialog(null, "Customer data has been recorded successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("Customer data has been recorded successfully");
+        } catch (HeadlessException | SQLException error) {
+            JOptionPane.showMessageDialog(null, "Unable to insert new customer data\n" + error.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
---
+++ src/MovieRental/ReadFile.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MovieRental;
+/**
+ * @author Tshepo Sepadile
+ * @author Banele Mlamleli
+ */
+import java.io.*;
+import java.util.*;
+public class ReadFile{
+
+    public static FileInputStream fileInputStreamCustomer;
+    public static FileInputStream fileInputStreamMovies;
+    public static FileInputStream fileInputStreamRental;
+    
+    public static ObjectInputStream objectInputStreamCustomer;
+    public static ObjectInputStream objectInputStreamMovies;
+    public static ObjectInputStream objectInputStreamRental;
+    
+    Customer customer;
+    DVD dvd;
+    Rental rental;
+    
+    //Arraylist to populate with data from the serialized object file
+    public static ArrayList<Customer> customerArraylist = new ArrayList<>();
+    public static ArrayList<DVD> dvdArraylist = new ArrayList<>();
+    public static ArrayList<Rental> rentalArraylist = new ArrayList<>();
+    
+    //read data into array lists
+    public void readCustomerSerializedData(){
+        try{
+            fileInputStreamCustomer = new FileInputStream("F:\\MovieRental\\src\\Assets\\Customers.ser");
+            objectInputStreamCustomer = new ObjectInputStream(fileInputStreamCustomer);
+            //reading data from Customer.ser file
+            while(true){
+                customer = (Customer) objectInputStreamCustomer.readObject();
+                customerArraylist.add(customer);
+            }
+        }catch(Exception e){
+            System.out.println("customer file successfully read");
+        }
+        
+        //Closing the files after being opened
+        try {
+            fileInputStreamCustomer.close();
+            objectInputStreamCustomer.close();
+        } catch (Exception error) {
+            System.out.println(error.getMessage()+", customer close code");
+        }
+    }
+    
+    //read data into Dvd array lists
+    public void readDvdSerializedData(){
+        try{
+            fileInputStreamMovies = new FileInputStream("F:\\MovieRental\\src\\Assets\\Movies.ser");
+            objectInputStreamMovies = new ObjectInputStream(fileInputStreamMovies);
+            
+            //reading data from Movies.ser file
+            while(true){
+                dvd = (DVD) objectInputStreamMovies.readObject();
+                dvdArraylist.add(dvd);
+            }
+        }catch(Exception e){
+            System.out.println("movies file successfully read");
+        }
+        //Closing the files after being opened
+        try {
+            objectInputStreamMovies.close();
+            fileInputStreamMovies.close();
+        } catch (Exception error) {
+            System.out.println(error.getMessage()+", movies close code");
+        }
+    }
+    
+    //read data into array lists
+    public void readRentalSerializedData(){
+        try{
+            fileInputStreamRental = new FileInputStream("F:\\MovieRental\\src\\Assets\\rental.ser");
+            objectInputStreamRental = new ObjectInputStream(fileInputStreamRental);
+            //reading data from Rental.ser file
+            while(true){
+                rental = (Rental) objectInputStreamRental.readObject();
+                rentalArraylist.add(rental);
+            }
+        }catch(Exception e){
+            System.out.println("rental file successfully read");
+        }
+        //Closing the files after being opened
+        try {
+            fileInputStreamRental.close();
+            objectInputStreamRental.close();
+        } catch (Exception error) {
+            System.out.println(error.getMessage()+", rental close code");
+        }
+    }
+}
